dfed4284c989132382bd1875a1554e84ad07889b
Harsh Patel <harsh.22427@knit.ac.in>
2026-01-09 21:46:46 +0530

    Completed task: Implemented REPL Loop

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 import sys
 
-
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    # pass
+    while True:  #infinite loop keeps running forever
+        # TODO: Uncomment the code below to pass the first stage
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        # pass
 
-    # taking user input
-    command = input()
-    print(f"{command}: command not found")
-
-
+        # taking user input
+        command = input().strip()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
